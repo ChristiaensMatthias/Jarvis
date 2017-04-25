9f0eb3de803bed241164afb1fab596bf874e21d1
Matthias Christiaens <matthias.christiaens@ida-mediafoundry.be>
2017-04-25 17:06:00 +0200

    - deleted client on server branch

--- js/bt/bluetooth.js
+++
@@ -1,5 +0,0 @@
-function btConnect(){
-    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-    .then(device => { /* ... */ })
-    .catch(error => { console.log(error); });
-}
