6566dbdc86a8931b7763229304a31aeccf72754f
Matthias Christiaens <matthias.christiaens@ida-mediafoundry.be>
2017-04-25 17:04:27 +0200

    - deleted server on client branch

--- app/models/groceryList.js
+++
@@ -1,8 +0,0 @@
-let mongoose     = require('mongoose');
-let Schema       = mongoose.Schema;
-
-let GroceryListSchema   = new Schema({
-    product: String
-});
-
-module.exports = mongoose.model('Grocery', GroceryListSchema);
