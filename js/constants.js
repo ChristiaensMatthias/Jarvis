564951a5b8f3476cd1c9a25f3c2c7f56364a7f33
Matthias Christiaens <matthias.christiaens@ida-mediafoundry.be>
2017-05-09 20:33:21 +0200

    - changed ip

--- js/constants.js
+++ js/constants.js
@@ -1,15 +1,15 @@
 SERVICE_URI = {
-    "SOCKET": "http://192.168.0.155:9000",
+    "SOCKET": "http://10.210.157.100:9000",
 
-    "WAKEONLAN": "http://192.168.0.155:9000/wakeonlan",
-    "TWITTER" : "http://192.168.0.155:9000/twitter",
+    "WAKEONLAN": "http://10.210.157.100:9000/wakeonlan",
+    "TWITTER" : "http://10.210.157.100:9000/twitter",
 
-    "GETGROCERYLIST" : "http://192.168.0.155:9000/getGroceryList",
-    "ADDGROCERYLIST": "http://192.168.0.155:9000/addToGroceryList",
-    "DELETEGROCERYLIST": "http://192.168.0.155:9000/deleteGroceryList",
-    "DELETEGROCERYITEM": "http://192.168.0.155:9000/deleteGroceryItem",
+    "GETGROCERYLIST" : "http://10.210.157.100:9000/getGroceryList",
+    "ADDGROCERYLIST": "http://10.210.157.100:9000/addToGroceryList",
+    "DELETEGROCERYLIST": "http://10.210.157.100:9000/deleteGroceryList",
+    "DELETEGROCERYITEM": "http://10.210.157.100:9000/deleteGroceryItem",
 
-    "YOUTUBESEARCH": "http://192.168.0.155:9000/ytsearch",
-    "AMPLIFIER": "http://192.168.0.155:9000/amplifier",
-    "AIRPLAY": "http://192.168.0.155:9000/airplay"
+    "YOUTUBESEARCH": "http://10.210.157.100:9000/ytsearch",
+    "AMPLIFIER": "http://10.210.157.100:9000/amplifier",
+    "AIRPLAY": "http://10.210.157.100:9000/airplay"
 };
